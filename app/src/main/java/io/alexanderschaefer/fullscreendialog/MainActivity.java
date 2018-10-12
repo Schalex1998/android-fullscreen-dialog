642fff48ba8b6d91ac03b6c12f0103647d577a1e
Alexander Schaefer <schalex1998@live.de>
2018-10-12 15:01:37 +0200

    initial

---
+++ app/src/main/java/io/alexanderschaefer/fullscreendialog/MainActivity.java
@@ -0,0 +1,23 @@
+package io.alexanderschaefer.fullscreendialog;
+
+import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MaterialButton button = findViewById(R.id.button);
+        button.setOnClickListener(v -> openDialog());
+    }
+
+    private void openDialog() {
+
+    }
+}
