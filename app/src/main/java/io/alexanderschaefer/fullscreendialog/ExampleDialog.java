d9b8f9e5d0ed00b1499f0c478d2732483571108a
Alexander Schaefer <schalex1998@live.de>
2018-10-12 18:05:32 +0200

    added full-screen dialog

---
+++ app/src/main/java/io/alexanderschaefer/fullscreendialog/ExampleDialog.java
@@ -0,0 +1,64 @@
+package io.alexanderschaefer.fullscreendialog;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+
+public class ExampleDialog extends DialogFragment {
+
+    public static final String TAG = "example_dialog";
+
+    private Toolbar toolbar;
+
+    public static ExampleDialog display(FragmentManager fragmentManager) {
+        ExampleDialog exampleDialog = new ExampleDialog();
+        exampleDialog.show(fragmentManager, TAG);
+        return exampleDialog;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.AppTheme_FullScreenDialog);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+            dialog.getWindow().setWindowAnimations(R.style.AppTheme_Slide);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.example_dialog, container, false);
+
+        toolbar = view.findViewById(R.id.toolbar);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        toolbar.setNavigationOnClickListener(v -> dismiss());
+        toolbar.setTitle("Some Title");
+        toolbar.inflateMenu(R.menu.example_dialog);
+        toolbar.setOnMenuItemClickListener(item -> {
+            dismiss();
+            return true;
+        });
+    }
+}
--- app/src/main/java/io/alexanderschaefer/fullscreendialog/MainActivity.java
+++ app/src/main/java/io/alexanderschaefer/fullscreendialog/MainActivity.java
@@ -18,6 +18,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openDialog() {
-
+        ExampleDialog.display(getSupportFragmentManager());
     }
 }
